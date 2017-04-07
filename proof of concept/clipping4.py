67e23c335743a0a7559e8663873adf478a685d1f
sigvaldm <sigvaldm@fys.uio.no>
2017-04-07 11:53:49 +0200

    Cleaned up

--- proof of concept/clipping4.py
+++ proof of concept/clipping4.py
@@ -9,25 +9,6 @@ import copy
 
 epsilon = 1e-10
 
-def plane_line_intersection(line, p, n):
-    v1 = line[0]
-    v2 = line[1]
-    normal_component = np.dot(v2-v1,n)
-
-    is_parallel = abs(normal_component) < epsilon
-    if is_parallel:
-        return None
-
-    alpha = np.dot(p-v1,n) / normal_component
-
-    # Include vertices _on_ the plane
-    intersects = alpha>-epsilon and alpha<1+epsilon
-    if not intersects:
-        return None
-
-    intersecting_point = v1+alpha*(v2-v1)
-    return intersecting_point
-
 def update_edge(edge, p, n):
     v1 = edge[0]
     v2 = edge[1]
@@ -61,29 +42,9 @@ def update_edge(edge, p, n):
 
     return edge, v_new
 
-def remove_non_unique_vertices(edge):
-    i = 0
-    while i<len(edge):
-        j = i + 1
-        while j<len(edge):
-            if vertices_equal(edge[i],edge[j]):
-                edge.pop(j)
-            else:
-                j += 1
-        i += 1
-
 def is_behind_plane(point, p, n):
     normal_component = np.dot(point-p,n)
     return normal_component < 0
-
-def is_in_front_of_plane(point, p, n):
-    """
-    Whether a point is in front of a plane, that is, whether it is on the
-    plane's side of which its normal vector n points. p is a point in the
-    plane such that the plane is described by the pair (p,n).
-    """
-    normal_component = np.dot(point-p,n)
-    return normal_component > 0
 
 
 # Vertex = [x,y]
@@ -157,17 +118,12 @@ class Polyhedron(object):
         edges = [list(a) for a in edges]
 
         self.faces = []
-        # self.faces.append([edges[3],edges[4],edges[5]])
-        # self.faces.append([edges[1],edges[2],edges[5]])
-        # self.faces.append([edges[0],edges[2],edges[4]])
-        # self.faces.append([edges[0],edges[1],edges[3]])
         self.faces.append([3,4,5])
         self.faces.append([1,2,5])
         self.faces.append([0,2,4])
         self.faces.append([0,1,3])
 
         for i,face in enumerate(self.faces):
-            # self.faces[i] = [copy.deepcopy(edges[e]) for e in face]
             for j,edge_id in enumerate(face):
                 self.faces[i][j] = copy.deepcopy(edges[edge_id])
 
@@ -212,10 +168,10 @@ class Polyhedron(object):
         plt.show()
 
     def clip(self, p, n):
-        assert self.vertices_are_unique(), "1"
-        print("\n\nCLIP")
+
         new_face = []
         for face_id,face in reversed(zip(count(),self.faces)):
+
             new_edge = []
             for edge_id,edge in reversed(zip(count(),face)):
 
@@ -227,40 +183,18 @@ class Polyhedron(object):
                 if new_vertex is not None:
                     new_edge.append(copy.deepcopy(new_vertex))
 
-                # new_vertex = plane_line_intersection(edge, p, n)
-                # edge0_in_front = is_in_front_of_plane(edge[0], p, n)
-                # if new_vertex is None: # No intersection
-                #     if edge0_in_front: # Remove the whole edge
-                #         face.pop(edge_id)
-                #         pass
-                # else:
-                #     new_edge.append(copy.deepcopy(new_vertex))
-                #     if edge0_in_front:
-                #         edge[0] = copy.deepcopy(new_vertex)
-                #     else:
-                #         edge[1] = copy.deepcopy(new_vertex)
-                # print("Face"+str(face_id)+": "+str(edge)+", new vertex:"+str(new_vertex))
-            #     assert self.vertices_are_unique(), "2"
-            #     assert self.vertex_coordinates_are_unique(), "a"
-            # remove_non_unique_vertices(new_edge)
-            # if len(new_edge)==1: new_edge = [] # edge of length zero
-
-
-            assert self.vertices_are_unique()
-            assert len(new_edge)==0 or len(new_edge)==2, str(len(new_edge))
+            assert len(new_edge)==0 or len(new_edge)==2
             if len(new_edge)==2 and edge_length(new_edge)>epsilon:
-                # assert edge_length(new_edge)>epsilon
                 face.append(new_edge)
                 new_face.append(copy.deepcopy(new_edge))
+
             if face == []:
                 self.faces.pop(face_id)
-                pass
-        assert self.vertices_are_unique(), "2"
-        if new_face != []:
+
+        if len(new_face)>2:
             self.faces.append(new_face)
-        assert self.vertices_are_unique(), "3"
 
-
+        assert self.vertices_are_unique()
 
 vertices = [np.array([0,0,0]),
             np.array([1,0,0]),
@@ -269,25 +203,10 @@ vertices = [np.array([0,0,0]),
 
 p = Polyhedron(*vertices)
 p.clip([0.5,0,0],[1,0,0])
-# p.clip([0.3,0,0],[1,0,0])
-# p.clip([0.1,0,0],[1,0,0])
-# print(p)
-# p.clip([0,0.3,0],[0,1,0])
+p.clip([0.3,0,0],[1,0,0])
+p.clip([0.1,0,0],[1,0,0])
+p.clip([0,0.3,0],[0,1,0])
 p.clip([0,0.5,0],[0,1,0])
 p.clip([0,0,0.5],[0,0,1])
 p.clip([0,0.4,0.4],[0,1,1])
 p.plot()
-
-# l = [0]*5
-# l[0] = [np.array([2,0],float),np.array([2,1],float)]
-# l[1] = [np.array([2,1],float),np.array([1,2],float)]
-# l[2] = [np.array([1,2],float),np.array([0,2],float)]
-# l[3] = [np.array([0,2],float),np.array([0,0],float)]
-# l[4] = [np.array([0,0],float),np.array([2,0],float)]
-# p = np.array([0,1],float)
-# n = np.array([0,1],float)
-
-# for i,a in enumerate(l):
-#     print("Old edge %d:"%i,a)
-#     edge, new_vertex = update_edge(a,p,n)
-#     print("New edge %d:"%i,edge,", New vertex:",new_vertex)
