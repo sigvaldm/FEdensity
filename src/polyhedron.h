67ba2fa3ff15af3c23ab93f7256a82dea51e80a9
sigvaldm <sigvaldm@fys.uio.no>
2017-04-24 09:42:53 +0200

    Added GPL license

--- src/polyhedron.h
+++ src/polyhedron.h
@@ -5,6 +5,23 @@
  *
  * Handles representation, cutting and volume computations of arbitrary
  * polyhedrons.
+ *
+ * Copyright 2017 Sigvald Marholm <marholm@marebakken.com>
+ *
+ * This file is part of FEdensity.
+ *
+ * FEdensity is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * FEdensity is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * FEdensity. If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef POLYHEDRON_H
 #define POLYHEDRON_H
